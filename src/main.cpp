887166c5d3b8fb92833029333aa2827436540087
Yuan Zhang <zhangyuan@users.noreply.github.com>
2023-01-08 20:26:05 +0800

    display humidity and temperature on oled with SSD1306

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,52 @@
 #include <Arduino.h>
 #include <DHT.h>
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
 #include "main.h"
 
 #define DHTTYPE DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
 
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+
+#define TEXT_SIZE 2
+#define ROW_HEIGHT 12 // row height = TEXT_SIZE * 6
+#define LINE_SPACING 10
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
 void setup()
 {
   Serial.begin(9600);
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;) ;
+  }
+
+  display.setTextSize(TEXT_SIZE);
+  display.setTextColor(WHITE);
+  display.cp437(true);
+
+  delay(2000);
+
   dht.begin();
 }
 
 void loop()
 {
+
   delay(2000);
+
   float h = dht.readHumidity();
   float t = dht.readTemperature();
   float f = dht.readTemperature(true);
@@ -38,4 +70,19 @@ void loop()
   Serial.print("Heat index: ");
   Serial.print(hi);
   Serial.println(" *F");
+
+  display.clearDisplay();
+  display.setCursor(0, 0);
+
+  display.println("H & T");
+  
+  display.setCursor(0, ROW_HEIGHT+LINE_SPACING+4);
+  display.print(h);
+  display.print(" %");
+
+  display.setCursor(0, (ROW_HEIGHT+LINE_SPACING) * 2);
+  display.print(t);
+  display.print(" *C ");
+
+  display.display();
 }
